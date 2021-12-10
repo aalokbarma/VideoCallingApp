5477c4d93f535a509ef04e5bd7b5ec5ca704c0c7
Aalok Barma <Aalokbarma17@gmail.com>
2021-12-10 20:54:43 +0530

    Getting Permissions

--- screens/callingScreen/index.tsx
+++ screens/callingScreen/index.tsx
@@ -1,12 +1,19 @@
 import { useNavigation, useRoute } from '@react-navigation/core';
-import React from 'react';
-import { View, Text } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { View, Text, PermissionsAndroid, Alert, Platform } from 'react-native';
 import CallingBottomButtons from '../../components/callingBottomButtons';
 import Styles from './styles';
 
 
+const permissions = [
+    PermissionsAndroid.PERMISSIONS.RECORD_AUDIO,
+    PermissionsAndroid.PERMISSIONS.CAMERA
+]
+
 
 const CallingScreen = () => {
+
+    const [permissionGranted, setPermissionGranted] = useState(false);
 
     const navigation = useNavigation();
     const route = useRoute();
@@ -18,6 +25,29 @@ const CallingScreen = () => {
     const onDecline = () => {
         navigation.navigate('Contacts');
     }
+
+    useEffect(() => {
+        const getPermission = async () => {
+            const granted = await PermissionsAndroid.requestMultiple(permissions);
+            const recordAudioGranted =
+            granted[PermissionsAndroid.PERMISSIONS.RECORD_AUDIO] = 'granted';
+            const cameraGranted =
+            granted[PermissionsAndroid.PERMISSIONS.CAMERA] = 'granted';
+            if (!cameraGranted || !recordAudioGranted) {
+            Alert.alert('Permissions not granted');
+            } else {
+            setPermissionGranted(true);
+ }
+        }
+
+        if (Platform.OS === 'android'){
+            getPermission();
+        }
+        else{
+            setPermissionGranted(true);
+        }
+
+    }, [])
 
     return (
         <View style = {Styles.page}>
--- screens/loginScreen/index.tsx
+++ screens/loginScreen/index.tsx
@@ -1,14 +1,57 @@
-import React, { useState } from 'react';
-import { View, Text, TextInput, Pressable } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { View, Text, TextInput, Pressable, Alert } from 'react-native';
 import Styles from './styles';
+import {Voximplant} from 'react-native-voximplant';
+
+import { APP_NAME, ACC_NAME } from '../../constants';
+import { useNavigation } from '@react-navigation/core';
 
 const LoginScreen = () => {
 
-    const [username, setUsername] = useState('')
-    const [password, setPassword] = useState('')
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
 
-    const onSubmit = () => {
-        console.warn('Login Successful')
+    const voximplant = Voximplant.getInstance();
+    const navigation = useNavigation();
+
+    useEffect (() => {
+        const connect = async () => {
+            const status = await voximplant.getClientState();
+            // console.log(status);
+            if (status === Voximplant.ClientState.DISCONNECTED){
+                await voximplant.connect();
+            }
+            else if (status === Voximplant.ClientState.LOGGED_IN) {
+                redirectHome();
+                }
+        };
+
+        connect();
+    }, [])
+
+    const onSubmit = async () => {
+        try{
+            const fqUsername = `${username}@${APP_NAME}.${ACC_NAME}.voximplant.com`;
+            await voximplant.login(fqUsername, password);
+            redirectHome()
+        } catch (e){
+            // console.log(e)
+            Alert.alert(e.name, `Error code: ${e.code}`)
+            setUsername('')
+            setPassword('')
+        }
+    }
+
+    const redirectHome = () => {
+        // navigation.navigate('Contacts')
+        navigation.reset({
+            index: 0,
+            routes: [
+                {
+                    name: 'Contacts'
+                },
+            ]
+        })
     }
 
     return (
